3df4d88129893751dcd95e61999aca0c134cd1b1
Loïc CAILLÉ <148439946+l0l0o@users.noreply.github.com>
2024-05-01 20:42:36 +0200

    item called from json

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const { createApp, ref } = Vue;
+import { cardComponent } from "./components/Card.js";
 
+const { createApp, ref } = Vue;
 const vm = createApp({
-  components: {},
+  components: {
+    "card-component": cardComponent,
+  },
   methods: {},
-  data() {},
+  data() {
+    return {
+      productList: [],
+    };
+  },
+  mounted() {
+    fetch("products.json")
+      .then((response) => response.json())
+      .then((data) => {
+        this.productList = data;
+      });
+  },
 }).mount("#app");
